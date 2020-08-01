60f388cb3d151b76c9a2afbb642c212514923f22
Alejandro Andrade <alex.anso.17@gmail.com>
2020-08-01 15:24:12 -0500

    Modificando unos errorsitos para mobile

--- src/components/ItemContent.js
+++ src/components/ItemContent.js
@@ -4,19 +4,21 @@ const ItemContent = ({ data }) => {
   console.log(data);
   return (
     <Fragment>
-      <h2>{data.title}</h2>
-      <p className="item-content">{data.description}</p>
-      <a
-        className="item-content hover-red-transform"
-        href={require(`../static/${data.fileName}`)}
-        download
-        style={{ textDecoration: "none" }}
-      >
-        <span role="img" aria-label="emoji-libretita`">
-          📝
-        </span>
-        Ver apunte y descargar
-      </a>
+      <div className="mb">
+        <h2>{data.title}</h2>
+        <p className="item-content">{data.description}</p>
+        <a
+          className="item-content hover-red-transform"
+          href={require(`../static/${data.fileName}`)}
+          download
+          style={{ textDecoration: "none" }}
+        >
+          <span role="img" aria-label="emoji-libretita`">
+            📝
+          </span>
+          Ver apunte y descargar
+        </a>
+      </div>
     </Fragment>
   );
 };
